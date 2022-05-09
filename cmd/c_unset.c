fbe9d2a75e7655ad43db7d890fe74cead19ff337
Abdessamad El ouali alami <ael-oual@e1r11p11.1337.ma>
2022-05-09 11:42:05 +0100

    some tests

---
+++ cmd/c_unset.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   c_unset.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-oual <ael-oual@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/09 07:22:59 by ael-oual          #+#    #+#             */
+/*   Updated: 2022/05/09 08:30:01 by ael-oual         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+// unset for delet var in env variables
+// export for inssrt a local var to environment
+//env is the environment variables in environment
+// best tutorial for this in https://www.digitalocean.com/community/tutorials/how-to-read-and-set-environmental-and-shell-variables-on-linux
+int main(int argc, char **argv , char **env)
+{
+    printf("lskd");
+}
