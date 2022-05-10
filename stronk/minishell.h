7079dd627d0abd6dde1ad3f88505e0e2e877f76b
Stronk_ <94312066+stronk-exe@users.noreply.github.com>
2022-05-10 22:30:17 +0100

    new commit--parsing...

--- stronk/minishell.h
+++ stronk/minishell.h
@@ -11,28 +11,40 @@
 
 typedef	struct	s_cmd
 {
-	char 	**cmd;
+	char	**env;
 	char	**path;
+	char 	**cmd;
+	char	**args;
+	char	**flags;
+	char	*sign;
+	int		input;
+	int		output;
 }				t_cmd;
 
-void	ft_echo(char *s, char **env);
+
 
 int		ft_exec(char **av, char **path, char **envp);
 char	*get_new_path(char **path, char **cmd, int x);
 
 // ----------------- path ---------------------//
-char	**get_path(char **envp);
+char	**get_path(void);
 
 
 // ---------------- utils --------------------//
 char	**ft_split(char *s, char c);
-char	*ft_strdup(const char *s1);
+char	*ft_strdup(char *s1);
 int		ft_strlen(const char *s);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_substr(char	*s, int start, int len);
-int		check_isvalid(char **path, char **cmd);
+int		check_isvalid(t_cmd *cmds/*char **path, char **cmd*/);
 
 // ---------------- parsing ------------------//
-int	ft_parsing(t_cmd *cmds, char *s, char **env);
+int		ft_parsing(t_cmd *cmds, char *s, char **envi);
+
+
+// --------------- commands -----------------//
+void	ft_echo(char *s, int x);
+void	ft_cd(char *path);
+void	ft_pwd(void);
 
 #endif
--- stronk/parsing/parsing.c
+++ stronk/parsing/parsing.c
@@ -6,21 +6,25 @@
 /*   By: ael-asri <ael-asri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 20:38:04 by ael-asri          #+#    #+#             */
-/*   Updated: 2022/05/10 14:01:54 by ael-asri         ###   ########.fr       */
+/*   Updated: 2022/05/10 22:11:04 by ael-asri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int	check_isvalid(char **path, char **cmd) {
+int	check_isvalid(t_cmd *cmds/*, char **path, char **cmd*/) {
 	int	i;
 
 	i = 0;
-	while (cmd[i] != '\0')
+	while (cmds->cmd[i] != '\0')
 	{
-		if (!get_new_path(path, cmd, i))
+		if (cmds->cmd[0][0] == '-')
+		{
+			cmds->flags = cmds->cmd;
+		}
+		else if (!get_new_path(cmds->path, cmds->cmd, i))
 		{	
-			printf("command not found %s\n", cmd[i]);
+			printf("command not found %s\n", cmds->cmd[i]);
 			return (0);
 		}
 		i++;
@@ -28,19 +32,56 @@ int	check_isvalid(char **path, char **cmd) {
 	return (1);
 }
 
-int	ft_parsing(t_cmd *cmds, char *s, char **env)
+int	is_cmd(char **path, char **cmd)
 {
-	cmds->path = get_path(env);
-	cmds->cmd = ft_split(s, ' ');
-	
-	if (!check_isvalid(cmds->path, cmds->cmd))
-	{
-		exit(1);
+	if (!get_new_path(path, cmd,0))
+	{	
+		printf("command not found %s\n", cmd[0]);
+		return (0);
 	}
-	// for (int i=0; cmds->path[i] != NULL ;i++)
-	// {
-	// 	printf("path is `%s`\n", cmds->path[i]);
-	// //	printf("cmd is `%s`\n", cmds->cmd[i]);
-	// }
 	return (1);
 }
+
+void	fill_args(t_cmd *cmds)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	j = 0;
+	while (cmds->cmd[i])
+	{
+		printf("%s\n", cmds->args[0]);
+		cmds->args[j] = cmds->cmd[i];
+		i++;
+		j++;
+	}
+}
+
+int	ft_parsing(t_cmd *cmds, char *s, char **envi)
+{
+	cmds->env = envi;
+	cmds->path = get_path();
+	cmds->cmd = ft_split(s, ' '/*, '\"', '\'', ';'*/);
+	
+	if (is_cmd(cmds->path, cmds->cmd))
+		fill_args(cmds);
+	else
+	{
+		printf("command not found %s\n", cmds->cmd[0]);
+		return (0);
+	}
+	for (int i=0; cmds->cmd[i] != NULL ;i++)
+	{
+		// printf("path is `%s`\n", cmds->path[i]);
+		printf("arg is `%s`\n", cmds->args[i]);
+	}
+	// if (!check_isvalid(cmds/*cmds->path, cmds->cmd*/))
+	// {
+	// 	exit(1);
+	// }
+	// printf("env is `%s`\n", cmds->env[1]);
+	
+	return (1);
+}
+
